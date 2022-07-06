d1a12bf0bfeee51ae1062ef3dccab3c64dbbab45
Andrew <41931259+aneeley05@users.noreply.github.com>
2022-07-06 17:34:55 +0000

    asdf

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+const connection = mysql.createConnection({
+    host: 'keap-emails.c68hykezydet.us-west-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'n33l3ylaw!',
+    database: 'sys'
+});
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    con.query("SELECT * FROM emails", function (err, result) {
+        if (err) res.send(err);
+        res.send(result);
+    });
 })
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
