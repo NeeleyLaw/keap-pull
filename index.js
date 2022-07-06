02044e3ec3da9ed68372a5bd5544b501fdf47b59
Andrew <41931259+aneeley05@users.noreply.github.com>
2022-07-06 19:10:43 +0000

    asdf

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const port = process.env.PORT || 3000;
 
-var running = false;
+const got = require("got");
+
+const { refreshKeap, initKeap, keapSession, serverUrl, keapClientID } = require('./oauthUtils.js');
+
+var running = true;
+
+// Last email in june has id 3354568
+var lastEmailInJuneID = 3354568;
 
 const con = mysql.createConnection({
     host: 'keap-emails.c68hykezydet.us-west-1.rds.amazonaws.com',
@@ -11,8 +19,6 @@ const con = mysql.createConnection({
     database: 'sys'
 });
 
-const port = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
     con.query("SELECT COUNT(*) FROM emails", function (err, result) {
         if (err) res.send(err);
@@ -20,13 +26,17 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/start", (req, res) => {
-    running = true;
+// Oauth and shit
+app.get('/keapsuccess', async (req, res) => {
+    initKeap(req.query.code);
     mainloop();
-    res.send("Started");
+    res.redirect("/");
+})
+app.get('/login', (req, res) => {
+    res.redirect(`https://accounts.infusionsoft.com/app/oauth/authorize?client_id=${keapClientID}&redirect_uri=${serverUrl}/keapsuccess&response_type=code`)
 })
 
-app.get("/start", (req, res) => {
+app.get("/stop", (req, res) => {
     running = false;
     res.send("Stopping");
 })
@@ -41,5 +51,46 @@ app.listen(port, () => {
 })
 
 async function mainloop() {
+    var newestFetchedEmailId = 0;
+    var newestFetchedRowIndex = -1;
+    con.query(`SELECT max(email_id) FROM emails`, function (err, result) {
+        if (err) throw err;
+        if (result[0]['max(email_id)'] != null) {
+            newestFetchedEmailId = result[0]['max(email_id)'];
+        }
+    })
+    con.query("SELECT COUNT(*) FROM emails", function (err, result) {
+        if (err) throw err;
+        newestFetchedRowIndex = (result[0]['COUNT(*)'] - 1)
+    });
 
+    for (let i = newestFetchedEmailId + 1; i < lastEmailInJuneID; i++) {
+        let thisEmail = getKeapEmail(i);
+        if(thisEmail && thisEmail != null) {
+            console.log(thisEmail)
+        }
+
+        //await timer(40);
+        await timer(4000);
+    }
+}
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function getKeapEmail(id) {
+    if(!keapSession.accessToken) {
+        return;
+    }
+    var res = await got.post(`https://api.infusionsoft.com/crm/rest/v1/emails/${id}`, {
+        throwHttpErrors: false,
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Bearer ${keapSession.accessToken}`
+        }
+    });
+    if (res.body.error) {
+        return;
+    }
+    return res.body;
 }
---
+++ oauthUtils.js
@@ -0,0 +1,45 @@
+// Utilities to help with OAuth in keap
+const got = require("got");
+
+var keapClientID = process.env.KEAP_CLIENT_ID;
+var keapClientSecret = process.env.KEAP_CLIENT_SECRET;
+var keapSession = {};
+
+var serverUrl = "https://" + process.env.CODESPACE_NAME + "-3000.githubpreview.dev";
+
+async function initKeap(code) {
+    var res = await got.post("https://api.infusionsoft.com/token", {
+        throwHttpErrors: false,
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=authorization_code&code=${code}&client_id=${keapClientID}&client_secret=${keapClientSecret}&redirect_uri=${serverUrl}/keapsuccess`,
+    });
+    if (res.body.error) {
+        console.log("KEAP Error getting access token: " + res.body.error);
+        return;
+    }
+    console.log("KEAP Access token: " + res.body.access_token);
+    keapSession.accessToken = res.body.access_token;
+}
+
+// Use refresh token to renew keap access token
+async function refreshKeap() {
+    var res = await got.post("https://api.infusionsoft.com/token", {
+        throwHttpErrors: false,
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=refresh_token&refresh_token=${keapSession.refreshToken}&client_id=${keapClientID}&client_secret=${keapClientSecret}`,
+    });
+    if (res.body.error) {
+        console.log("KEAP Error refreshing access token: " + res.body.error);
+        return;
+    }
+    console.log("KEAP Access token: " + res.body.access_token);
+    keapSession.accessToken = res.body.access_token;
+}
+
+module.exports = { refreshKeap, initKeap, keapSession, serverUrl, keapClientID };
