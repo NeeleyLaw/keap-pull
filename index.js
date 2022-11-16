074c60f595139f5f70967f8c82b1facb59fbafe1
Andrew Neeley <41931259+aneeley05@users.noreply.github.com>
2022-11-16 22:17:58 +0000

    asdf

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 const got = require("got");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { refreshKeap, initKeap, keapSession, serverUrl, keapClientID } = require('./oauthUtils.js');
+const {
+    refreshKeap,
+    initKeap,
+    keapSession,
+    serverUrl,
+    keapClientID,
+} = require("./oauthUtils.js");
 
 var running = true;
 
@@ -14,14 +20,14 @@ var pulledEmails = 0;
 var lastEmailInJuneID = 3354568;
 
 const con = mysql.createConnection({
-    host: 'keap-emails.c68hykezydet.us-west-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'n33l3ylaw!',
-    database: 'sys'
+    host: "keap-emails.c68hykezydet.us-west-1.rds.amazonaws.com",
+    user: "admin",
+    password: "n33l3ylaw!",
+    database: "sys",
 });
 
 app.get("/", (req, res) => {
-    if(pulledEmails != 0) {
+    if (pulledEmails != 0) {
         res.send("Emails pulled: " + pulledEmails);
     } else {
         res.send("Email count still loading...");
@@ -29,61 +35,81 @@ app.get("/", (req, res) => {
 });
 
 // Oauth and shit
-app.get('/keapsuccess', async (req, res) => {
+app.get("/keapsuccess", async(req, res) => {
     initKeap(req.query.code);
     mainloop();
     res.redirect("/");
-})
-app.get('/login', (req, res) => {
-    res.redirect(`https://accounts.infusionsoft.com/app/oauth/authorize?client_id=${keapClientID}&redirect_uri=${serverUrl}/keapsuccess&response_type=code`)
-})
+});
+app.get("/login", (req, res) => {
+    res.redirect(
+        `https://accounts.infusionsoft.com/app/oauth/authorize?client_id=${keapClientID}&redirect_uri=${serverUrl}/keapsuccess&response_type=code`
+    );
+});
 
 app.get("/stop", (req, res) => {
     running = false;
-    console.log("TERMINATED")
+    console.log("TERMINATED");
     res.send("Stopping");
-})
+});
 
-con.connect(function (err) {
+con.connect(function(err) {
     if (err) console.log(err);
     console.log("Connected!");
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
 
 async function mainloop() {
     var newestFetchedEmailId = 0;
 
-    con.query(`SELECT max(email_id) FROM emails`, function (err, result) {
+    con.query(`SELECT max(email_id) FROM emails`, function(err, result) {
         if (err) throw err;
-        if (result[0]['max(email_id)'] != null) {
-            newestFetchedEmailId = result[0]['max(email_id)'];
+        if (result[0]["max(email_id)"] != null) {
+            newestFetchedEmailId = result[0]["max(email_id)"];
         }
-    })
+    });
 
     await timer(2000);
 
     console.log("Starting at email id: " + (newestFetchedEmailId + 2));
 
     console.log("Running...");
-    var i = newestFetchedEmailId + 2
+    var i = newestFetchedEmailId + 2;
     var mainInterval = setInterval(() => {
         if (!running || i > lastEmailInJuneID) {
             console.log("DONE");
             clearInterval(mainInterval);
         }
         getKeapEmail(i).then((thisEmail) => {
-            if (thisEmail && thisEmail != null && !thisEmail.message && !thisEmail.fault) {
-                if(thisEmail.contact_id != 0) addEmailToDB(thisEmail.id, thisEmail.contact_id, thisEmail.subject, thisEmail.headers, thisEmail.plain_content, thisEmail.html_content, thisEmail.sent_to_address, thisEmail.sent_from_address, thisEmail.sent_to_cc_addresses, thisEmail.sent_date, thisEmail.received_date);
-                else console.log("Zero contact id for email " + thisEmail.id)
+            if (
+                thisEmail &&
+                thisEmail != null &&
+                !thisEmail.message &&
+                !thisEmail.fault
+            ) {
+                if (thisEmail.contact_id != 0)
+                    addEmailToDB(
+                        thisEmail.id,
+                        thisEmail.contact_id,
+                        thisEmail.subject,
+                        thisEmail.headers,
+                        thisEmail.plain_content,
+                        thisEmail.html_content,
+                        thisEmail.sent_to_address,
+                        thisEmail.sent_from_address,
+                        thisEmail.sent_to_cc_addresses,
+                        thisEmail.sent_date,
+                        thisEmail.received_date
+                    );
+                else console.log("Zero contact id for email " + thisEmail.id);
             } else if (thisEmail.message) {
                 i -= 1;
             }
-        })
+        });
         i += 2;
-    }, 600)
+    }, 600);
 
     // var countInterval = setInterval(() => {
     //     con.query("SELECT COUNT(*) FROM emails", function (err, result) {
@@ -93,40 +119,60 @@ async function mainloop() {
     // }, 300000)
 }
 
-const timer = ms => new Promise(res => setTimeout(res, ms))
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 async function getKeapEmail(id) {
     if (!keapSession.accessToken) {
         return;
     }
-    var res = await got.get(`https://api.infusionsoft.com/crm/rest/v1/emails/${id}`, {
-        throwHttpErrors: false,
-        responseType: "json",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": `Bearer ${keapSession.accessToken}`
+    var res = await got.get(
+        `https://api.infusionsoft.com/crm/rest/v1/emails/${id}`, {
+            throwHttpErrors: false,
+            responseType: "json",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: `Bearer ${keapSession.accessToken}`,
+            },
         }
-    });
+    );
     if (res.body.error) {
         return;
     }
     return res.body;
 }
 
-async function addEmailToDB(email_id, contact_id, subject, headers, plain_content, html_content, sent_to_address, sent_from_address, sent_to_cc_addresses, sent_date, received_date) {
-    if (subject != null) subject = '"' + subject.replaceAll("\"", "") + '"';
-    if (plain_content != null) plain_content = '"' + plain_content.replaceAll("\"", "") + '"';
-    if (html_content != null) html_content = '"' + html_content.replaceAll("\"", "") + '"';
-    if (sent_to_address != null) sent_to_address = '"' + sent_to_address.replaceAll("\"", "") + '"';
-    if (sent_from_address != null) sent_from_address = '"' + sent_from_address.replaceAll("\"", "") + '"';
-    if (sent_to_cc_addresses != null) sent_to_cc_addresses = '"' + sent_to_cc_addresses.replaceAll("\"", "") + '"';
-    if (headers != null) headers = '"' + headers.replaceAll("\"", "") + '"';
-    if (sent_date != null) sent_date = '"' + sent_date.replaceAll("\"", "") + '"';
-    if (received_date != null) received_date = '"' + received_date.replaceAll("\"", "") + '"';
+async function addEmailToDB(
+    email_id,
+    contact_id,
+    subject,
+    headers,
+    plain_content,
+    html_content,
+    sent_to_address,
+    sent_from_address,
+    sent_to_cc_addresses,
+    sent_date,
+    received_date
+) {
+    if (subject != null) subject = '"' + subject.replaceAll('"', "") + '"';
+    if (plain_content != null)
+        plain_content = '"' + plain_content.replaceAll('"', "") + '"';
+    if (html_content != null)
+        html_content = '"' + html_content.replaceAll('"', "") + '"';
+    if (sent_to_address != null)
+        sent_to_address = '"' + sent_to_address.replaceAll('"', "") + '"';
+    if (sent_from_address != null)
+        sent_from_address = '"' + sent_from_address.replaceAll('"', "") + '"';
+    if (sent_to_cc_addresses != null)
+        sent_to_cc_addresses = '"' + sent_to_cc_addresses.replaceAll('"', "") + '"';
+    if (headers != null) headers = '"' + headers.replaceAll('"', "") + '"';
+    if (sent_date != null) sent_date = '"' + sent_date.replaceAll('"', "") + '"';
+    if (received_date != null)
+        received_date = '"' + received_date.replaceAll('"', "") + '"';
 
     var query = `INSERT INTO emails (email_id, contact_id, subject, headers, plain_content, html_content, sent_to_address, sent_from_address, sent_date, received_date, sent_to_cc_addresses) VALUES (${email_id}, ${contact_id}, ${subject}, ${headers}, ${plain_content}, ${html_content}, ${sent_to_address}, ${sent_from_address}, ${sent_date}, ${received_date}, ${sent_to_cc_addresses})`;
 
-    con.query(query, function (err, result) {
+    con.query(query, function(err, result) {
         if (err) console.log(err);
     });
 }
